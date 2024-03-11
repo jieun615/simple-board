fd66cc4ea16b53e1176ec1cf17243c0570d80a5f
jieun615 <je060105@gmail.com>
2024-03-11 22:01:39 +0900

    게시글 삭제 구현

---
+++ src/board/board.controller.ts
@@ -0,0 +1,41 @@
+import { BoardService } from './board.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Injectable,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+@Controller('board')
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  @Get()
+  findAll() {
+    return this.boardService.findAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return this.boardService.find(Number(id));
+  }
+
+  @Post()
+  create(@Body() data) {
+    return this.boardService.create(data);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data) {
+    return this.boardService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.boardService.delete(Number(id));
+  }
+}
---
+++ src/board/board.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BoardService {
+  private boards = [
+    {
+      title: 'Hello world',
+      content: 'Content 1',
+      id: 1,
+    },
+    {
+      title: 'Hello world!',
+      content: 'Content 2',
+      id: 2,
+    },
+    {
+      title: 'Hello world!',
+      content: 'Content 3',
+      id: 3,
+    },
+  ];
+  findAll() {
+    return this.boards;
+  }
+
+  find(id: number) {
+    const index = this.getBoardId(id);
+    return this.boards[index];
+  }
+
+  create(data) {
+    const newBoard = { id: this.getNextId(), ...data };
+    this.boards.push(newBoard);
+    return newBoard;
+  }
+
+  update(id: number, data) {
+    const index = this.getBoardId(id);
+    if (index > -1) {
+      this.boards[index] = {
+        ...this.boards[index],
+        ...data,
+      };
+      return this.boards[index];
+    }
+    return null;
+  }
+
+  delete(id: number) {
+    const index = this.getBoardId(id);
+
+    if (index > -1) {
+      const deleteBoard = this.boards[index];
+      this.boards.splice(index, 1);
+      return deleteBoard;
+    }
+    return null;
+  }
+
+  getBoardId(id: number) {
+    return this.boards.findIndex((board) => board.id === id);
+  }
+
+  getNextId() {
+    return this.boards.sort((a, b) => (b.id - a.id))[0].id + 1;
+  }
+}
