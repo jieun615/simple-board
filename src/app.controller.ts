7b253126adbd23d123fd7caa1ffc133634b66a51
jieun615 <je060105@gmail.com>
2024-03-12 23:35:32 +0900

    환경변수 설정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,14 +7,19 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ip } from './decorators/ip.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get()
   getHello(@Ip() ip: string): string {
     //return this.appService.getHello();
+    this.configService.get<string>('ENVIRONMENT');
     throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
   }
 
