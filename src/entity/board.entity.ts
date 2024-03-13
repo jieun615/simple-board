b481cea20a23e53200615d5b0ef54f6cb91120ed
jieun615 <je060105@gmail.com>
2024-03-13 20:31:43 +0900

    swagger 추가

--- src/entity/board.entity.ts
+++ src/entity/board.entity.ts
@@ -1,25 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
   Entity,
-  PrimaryColumn,
+  PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
 export class Board {
-  @PrimaryColumn({ name: 'id' })
+  @PrimaryGeneratedColumn({ name: 'id' })
   id: number;
 
+  @ApiProperty({ description: 'user_id' })
   @Column()
-  name: string;
+  userId: number;
 
+  @ApiProperty({ description: '내용' })
   @Column()
   contents: string;
 
+  @ApiProperty({ description: '수정일' })
   @UpdateDateColumn()
   updateAt: Date;
 
+  @ApiProperty({ description: '생성일' })
   @CreateDateColumn()
   createdAt: Date;
 }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,16 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ description: '유저아이디', example: 'admin' })
   @Column({ unique: true })
   username: string;
 
+  @ApiProperty({ description: '유저아이디' })
   @Column({ select: false })
   password: string;
 
+  @ApiProperty({ description: '이름' })
   @Column()
-  name: string; 
+  name: string;
 }
