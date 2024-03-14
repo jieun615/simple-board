6d87cb156582c4bb23c37d0d0ba96af94694144b
jieun615 <je060105@gmail.com>
2024-03-14 19:25:56 +0900

    User Entity 구현

--- src/routes/user/user.service.ts
+++ src/routes/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from 'src/entity/board.entity';
 import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
@@ -8,7 +10,17 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
   async getUser() {
-    return this.userRepository.find();
+    const qb = this.userRepository.createQueryBuilder();
+
+    qb.addSelect((subQuery) => {
+      return subQuery
+        .select('count(id)')
+        .from(Board, 'Board')
+        .where('Board.userId = User.id');
+    }, 'User_boardCount');
+
+    return qb.getMany();
   }
 }
